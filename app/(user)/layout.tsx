891d0a2c8dcd60819f8a21a866d7f8c10287edde
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2024-09-07 21:26:35 +0545

    feat: adding loading screen

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,9 +1,10 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, Suspense } from 'react';
+import Loading from './loading';
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
     <div className='container mx-auto max-w-screen-xl py-10 px-2.5 lg:px-20'>
-      {children}
+      <Suspense fallback={<Loading />}>{children}</Suspense>
     </div>
   );
 }
