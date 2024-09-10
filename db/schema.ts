aac5490f9f299f8d354e58b367362765c00220ed
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2024-09-10 21:49:22 +0545

    feat: creating feedback schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,9 +1,30 @@
-import { pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm';
+import { integer, pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
 
 export const projects = pgTable('projects', {
   id: serial('id').primaryKey(),
   name: text('name'),
   description: text('description'),
   url: text('url'),
-  userId: varchar('user_id')
+  userId: varchar('user_id'),
 });
+
+export const projectsRelations = relations(projects, ({ many }) => ({
+  feedbacks: many(feedbacks),
+}));
+
+export const feedbacks = pgTable('feedbacks', {
+  id: serial('id').primaryKey(),
+  projectId: integer('project_id'),
+  userName: text('user_name'),
+  userEmail: text('user_email'),
+  message: text('message'),
+  rating: integer('rating'),
+});
+
+export const feedbacksRelations = relations(feedbacks, ({ one }) => ({
+  projects: one(projects, {
+    fields: [feedbacks.projectId],
+    references: [projects.id],
+  }),
+}));
